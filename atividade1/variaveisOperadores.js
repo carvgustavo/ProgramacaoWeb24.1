f94138a70f355578d09cb4c424a3a52383230437
Gustavo Carvalho <127608949+carvgustavo@users.noreply.github.com>
2024-03-13 09:47:49 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let num1 = 15
+let num2 = 20
+
+let somar = num1 + num2
+let subtrair = num1 - num2 
+let multiplicar = num1 * num2
+let dividir = num1 / num2
+
+console.log(`Resultado da soma de ${num1} e ${num2}: ${somar}`)
+console.log(`Resultado da subtracao de ${num1} e ${num2}: ${subtrair}`)
+console.log(`Resultado da multiplicacao de ${num1} e ${num2}: ${multiplicar}`)
+console.log(`Resultado da divisao de ${num1} e ${num2}: ${dividir}`)
